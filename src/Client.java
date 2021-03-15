193914e8cad775a0ce67b78354020731b9288e19
EternalEffy <effyjeev@gmail.com>
2021-03-15 21:56:09 +0300

    внесла изменения в соотвествии с требованиями

--- src/Client.java
+++ src/Client.java
@@ -1,27 +1,36 @@
 import java.io.*;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class Client {
     private static Socket client;
+    private static BufferedReader in;
     private static BufferedWriter out;
-
+    private static String str = "Меня зовут так то";
 
     public static void main(String[] args) {
-        try{
+
             try {
                 client = new Socket("localhost", 3310);
                 out = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+                in = new BufferedReader(new InputStreamReader(client.getInputStream()));
 
-                out.write("Привет" + "\n");
+                out.write( str.getBytes("UTF-8") + "\n");
                 out.flush();
-            }
-            finally {
-                client.close();
-                out.close();
-            }
+                System.out.println(in.readLine());
+
         } catch (IOException e) {
             e.printStackTrace();
         }
+            finally {
+                    try {
+                     client.close();
+                     in.close();
+                     out.close();
+                 } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
     }
 
 }
